a2c1c6a86319361f1b265c9d11b1100f37001b2a
ksbdigital <kevin_binks@hotmail.com>
2014-06-29 11:27:31 +0100

    example update

--- config/connectionexample.php
+++ config/connectionexample.php
@@ -1,6 +1,6 @@
 <?php 
 
-# Database connection exampl:
+# Database connection example:
 $dbc = mysqli_connect('localhost', 'user' , 'password', 'database') OR die('Could not connect becasue:' .mysqli_connect_error());
 
 ?>
